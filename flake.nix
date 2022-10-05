b57ae57334bf79b2cfbec949bfd2bd17ad6964cb
Maxwell L-T <maxwell.lt@live.com>
2022-10-05 17:37:06 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "modestly-modular-modpack-modifier";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+	pkgs = import nixpkgs {
+	  inherit system overlays;
+	};
+	rust = pkgs.rust-bin.stable.latest.default;
+	rustPlatform = pkgs.makeRustPlatform {
+	  rustc = rust;
+	  cargo = rust;
+	};
+      in with pkgs; {
+        devShell = mkShell {
+	  buildInputs = [
+	    rust
+            diffutils
+            rust-analyzer
+	  ];
+
+	  shellHook = ''
+	    export RUST_BACKTRACE=1
+	  '';
+	};
+
+	defaultPackage = rustPlatform.buildRustPackage {
+	  pname = "modestly-modular-modpack-modifier";
+	  version = "0.1.0";
+
+	  src = builtins.filterSource
+	    (path: type: type != "symlink" && baseNameOf path != "target")
+	    ./.;
+
+	  doCheck = false;  # The tests require internet access.
+
+	  cargoSha256 = "sha256-4ri7iGZXHkUyrfQw5qA4qUEdTYWyV0gyDl21PvipEJg=";
+	};
+  });
+}
