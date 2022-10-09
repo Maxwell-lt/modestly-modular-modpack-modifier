0ad940ff2f349bf8599a4385b704851c0a88a5f6
Maxwell L-T <maxwell.lt@live.com>
2022-10-09 09:17:41 -0400

    Build graph representations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use std::collections::{HashMap, BTreeMap};
 use std::fs::File;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use operators::model::Operators;
-use operators::operators::Operator;
 use options::Options;
 use walkdir::WalkDir;
 
+use crate::operators::model::OperatorConfig;
 use crate::operators::operators::*;
 
 
@@ -14,33 +15,68 @@ mod options;
 
 fn main() -> Result<()> {
     let opt: Options = options::parse();
-    let config: Operators = serde_yaml::from_reader(File::open(&opt.config_file)?)?;
+    let config: Operators = serde_yaml::from_reader(
+        File::open(&opt.config_file).context(format!("Failed to open config file {}", opt.config_file.to_string_lossy()))?)
+        .context("Failed to parse YAML")?;
 
     println!("{:?}", config);
+
+    let config_map: HashMap<String, OperatorConfig> = config.operators.
+        into_iter()
+        .map(|o| (o.get_name(), o))
+        .collect();
+
+    let adjacency_list: BTreeMap<String, Vec<String>> = config_map.iter()
+        .map(|(k, v)| (k.to_owned(), v.get_refs()))
+        .collect();
+
+    let adjacency_matrix = create_adjacency_matrix(&adjacency_list)?;
+
+    println!("{:?}\n{:?}", adjacency_list, adjacency_matrix);
     
-    let uri = URILiteral::new("https://cdn.modrinth.com/data/p87Jiw2q/versions/6D8o98Bp/LostEra_modpack_1.5.2a.mrpack".to_string());
-    let regex = RegexLiteral::new("modrinth\\.index\\.json|overrides/config/NuclearCraft/ToolConfig\\.cfg".to_string())?;
+    //let uri = URILiteral::new("https://cdn.modrinth.com/data/p87Jiw2q/versions/6D8o98Bp/LostEra_modpack_1.5.2a.mrpack".to_string());
+    //let regex = RegexLiteral::new("modrinth\\.index\\.json|overrides/config/NuclearCraft/ToolConfig\\.cfg".to_string())?;
 
-    let downloader = ArchiveDownloader::new(uri.output()?)?;
-    let filter = ArchiveFilter::new(regex.output()?, downloader.output()?)?;
+    //let downloader = ArchiveDownloader::new(uri.output()?)?;
+    //let filter = ArchiveFilter::new(regex.output()?, downloader.output()?)?;
 
-    println!("Downloaded files:");
-    let tempdir = downloader.output()?;
-    for file in WalkDir::new(tempdir.path()) {
-        let file = file?;
-        println!("{}", file.path().to_string_lossy());
-    }
+    //println!("Downloaded files:");
+    //let tempdir = downloader.output()?;
+    //for file in WalkDir::new(tempdir.path()) {
+    //    let file = file?;
+    //    println!("{}", file.path().to_string_lossy());
+    //}
 
-    println!("Filtered files:");
-    let tempdir = filter.output()?;
-    for file in WalkDir::new(tempdir.path()) {
-        let file = file?;
-        println!("{}", file.path().to_string_lossy());
-    }
+    //println!("Filtered files:");
+    //let tempdir = filter.output()?;
+    //for file in WalkDir::new(tempdir.path()) {
+    //    let file = file?;
+    //    println!("{}", file.path().to_string_lossy());
+    //}
 
-    let outpath = PathLiteral::new("./output".to_string());
-    println!("{}", outpath.output()?.as_path().canonicalize()?.to_string_lossy());
-    FileWriter::new(filter.output()?, &outpath.output()?)?;
+    //let outpath = PathLiteral::new("./output".to_string());
+    //println!("{}", outpath.output()?.as_path().canonicalize()?.to_string_lossy());
+    //FileWriter::new(filter.output()?, &outpath.output()?)?;
 
     Ok(())
+}
+
+fn create_adjacency_matrix(map: &BTreeMap<String, Vec<String>>) -> Result<Vec<Vec<bool>>> {
+    let size = map.len();
+    let mut matrix: Vec<Vec<bool>> = Vec::with_capacity(size);
+    for _ in 0..size {
+        matrix.push(vec![false; size]);
+    }
+
+    let indices: Vec<&String> = map.keys().collect();
+    for (i, edges) in map.values().enumerate() {
+        for edge in edges {
+            matrix[i][indices
+                .binary_search(&edge).ok()
+                .context(format!("Failed to build adjacency matrix, node with name {} referenced by {} does not exist", edge, indices[i]))?
+            ] = true;
+        }
+    }
+
+    Ok(matrix)
 }
--- src/operators/model.rs
+++ src/operators/model.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "kind")]
-pub enum Operator {
+pub enum OperatorConfig {
     // Literals
     URI { name: String, value: String },
     Path { name: String, value: String },
@@ -17,5 +17,33 @@ pub enum Operator {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Operators {
-    pub operators: Vec<Operator>,
+    pub operators: Vec<OperatorConfig>,
+}
+
+impl OperatorConfig {
+    pub fn get_name(&self) -> String {
+        match self {
+            Self::URI { name, value } => name.to_owned(),
+            Self::Path { name, value } => name.to_owned(),
+            Self::Number { name, value } => name.to_owned(),
+            Self::Text { name, value } => name.to_owned(),
+            Self::Regex { name, value } => name.to_owned(),
+            Self::ArchiveDownloader { name, uri } => name.to_owned(),
+            Self::ArchiveFilter { name, archive, path_regex } => name.to_owned(),
+            Self::FileWriter { name, archive, destination } => name.to_owned(),
+        }
+    }
+
+    pub fn get_refs(&self) -> Vec<String> {
+        match self {
+            Self::URI { name, value } => Vec::new(),
+            Self::Path { name, value } => Vec::new(),
+            Self::Number { name, value } => Vec::new(),
+            Self::Text { name, value } => Vec::new(),
+            Self::Regex { name, value } => Vec::new(),
+            Self::ArchiveDownloader { name, uri } => vec![uri.to_owned()],
+            Self::ArchiveFilter { name, archive, path_regex } => vec![archive.to_owned(), path_regex.to_owned()],
+            Self::FileWriter { name, archive, destination } => vec![archive.to_owned(), destination.to_owned()],
+        }
+    }
 }
