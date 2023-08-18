e03189a890e58f5405613b75e5cd86ecbb4b9769
Maxwell L-T <maxwell.lt@live.com>
2023-08-18 11:39:50 -0400

    Implement methods for DiContainer

--- src/di/container.rs
+++ src/di/container.rs
@@ -8,7 +8,7 @@ pub(crate) struct DiContainer {
     // Global config values (e.g. API keys)
     configs: HashMap<String, String>,
     // Broadcast channel handles
-    channels: HashMap<String, OutputType>,
+    channels: HashMap<ChannelId, InputType>,
     // Global FileStore object
     filestore: FileStore,
     // Triggers all nodes to begin waiting for inputs
@@ -19,20 +19,127 @@ pub(crate) struct DiContainer {
     waker: broadcast::Sender<()>,
 }
 
-pub(crate) enum OutputType {
+#[derive(Debug, Clone)]
+pub(crate) enum InputType {
     Text(broadcast::Sender<String>),
     Path(broadcast::Sender<FilePath>),
     Files(broadcast::Sender<FileTree>),
     Regex(broadcast::Sender<Regex>),
 }
 
+impl InputType {
+    fn subscribe(&self) -> OutputType {
+        match self {
+            InputType::Text(c) => OutputType::Text(c.subscribe()),
+            InputType::Path(c) => OutputType::Path(c.subscribe()),
+            InputType::Files(c) => OutputType::Files(c.subscribe()),
+            InputType::Regex(c) => OutputType::Regex(c.subscribe()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum OutputType {
+    Text(broadcast::Receiver<String>),
+    Path(broadcast::Receiver<FilePath>),
+    Files(broadcast::Receiver<FileTree>),
+    Regex(broadcast::Receiver<Regex>),
+}
+
+/// Stores a channel ID by a tuple of (output node name, output name)
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub(crate) struct ChannelId(String, String);
+
 impl DiContainer {
-    pub(crate) fn new(configs: HashMap<String, String>, channels: HashMap<String, OutputType>) -> Self {
+    pub(crate) fn new(configs: HashMap<String, String>, channels: HashMap<ChannelId, InputType>) -> Self {
         Self { configs, channels, filestore: FileStore::new(), waker: broadcast::channel::<()>(1).0 }
+    }
+
+    pub(crate) fn get_filestore(&self) -> FileStore {
+        self.filestore.clone()
+    }
+
+    pub(crate) fn get_config(&self, config: &str) -> Option<String> {
+        self.configs.get(config).cloned()
+    }
+
+    pub(crate) fn get_sender(&self, id: &ChannelId) -> Option<InputType> {
+        self.channels.get(id).cloned()
+    }
+
+    pub(crate) fn get_receiver(&self, id: &ChannelId) -> Option<OutputType> {
+        self.channels.get(id).map(|c| c.subscribe())
+    }
+
+    pub(crate) fn get_waker(&self) -> broadcast::Receiver<()> {
+        self.waker.subscribe()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn filestore_is_cloned() {
+        let c = DiContainer::new(HashMap::new(), HashMap::new());
+        let fs1 = c.get_filestore();
+        let fs2 = c.get_filestore();
+        let hash = fs1.write_file("Hello World!".into());
+
+        // Test that both filestores point to the same underlying data
+        assert!(fs2.get_file(hash).is_some());
+    }
+
+    #[test]
+    fn get_config() {
+        let c = DiContainer::new(HashMap::from([("curse.api-key".into(), "12345678".into())]), HashMap::new());
+        
+        assert_eq!(c.get_config("curse.api-key").unwrap(), "12345678");
+    }
+
+    #[test]
+    fn get_sender() {
+        let (tx, mut rx) = broadcast::channel::<String>(1);
+        let channel_id = ChannelId("node1".into(), "outputA".into());
+        let c = DiContainer::new(HashMap::new(), HashMap::from([
+                                                               (channel_id.clone(), InputType::Text(tx))
+        ]));
+
+        let container_tx = match c.get_sender(&channel_id).unwrap() {
+            InputType::Text(channel) => channel,
+            _ => unreachable!(),
+        };
+        container_tx.send("Test".into()).unwrap();
+
+        assert_eq!(rx.try_recv().unwrap(), "Test");
+    }
+
+    #[test]
+    fn get_receiver() {
+        let tx = broadcast::channel::<String>(1).0;
+        let channel_id = ChannelId("node1".into(), "outputA".into());
+        let c = DiContainer::new(HashMap::new(), HashMap::from([
+                                                               (channel_id.clone(), InputType::Text(tx.clone()))
+        ]));
+
+        let mut container_rx = match c.get_receiver(&channel_id).unwrap() {
+            OutputType::Text(channel) => channel,
+            _ => unreachable!(),
+        };
+        tx.send("Test".into()).unwrap();
+        
+        assert_eq!(container_rx.try_recv().unwrap(), "Test");
+    }
+
+    #[test]
+    fn get_waker_channel() {
+        let c = DiContainer::new(HashMap::new(), HashMap::new());
+
+        let mut waker_rx = c.get_waker();
+        let waker_tx = c.waker.clone();
+        waker_tx.send(()).unwrap();
+
+        assert!(waker_rx.try_recv().is_ok());
+    }
 }
