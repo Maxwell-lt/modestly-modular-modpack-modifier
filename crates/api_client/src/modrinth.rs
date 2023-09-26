d305cc56b8c0f145cc3e66215bb4da4d29d2feb4
Maxwell L-T <maxwell.lt@live.com>
2023-09-26 19:21:31 -0400

    Clean up Modrinth client code

--- crates/api_client/src/modrinth.rs
+++ crates/api_client/src/modrinth.rs
@@ -1,9 +1,4 @@
-use std::{sync::Arc, time::Duration};
-
-use ratelimit::Ratelimiter;
-use ureq::{Agent, AgentBuilder};
-
-use super::common::{ApiClient, ApiClientBuilder, USER_AGENT};
+use super::common::{ApiClient, ApiClientBuilder};
 
 static MODRINTH_BASE_URL: &str = "https://api.modrinth.com/v2";
 
@@ -12,19 +7,11 @@ pub struct ModrinthClient {
     client: ApiClient,
 }
 
-/// Modrinth has a documented rate limit of 300 requests per minute.
-/// Using a slightly lower limit to avoid having to deal with rate limit headers.
-fn get_ratelimit() -> Ratelimiter {
-    Ratelimiter::builder(295, Duration::from_secs(60))
-        .max_tokens(295)
-        .initial_available(295)
-        .build()
-        .unwrap()
-}
-
 impl ModrinthClient {
     /// Get a [`ModrinthClient`] that uses the official Modrinth API.
     pub fn new() -> Self {
+        // Modrinth has a documented rate limit of 300 requests per minute.
+        // Using a slightly lower limit of 295 to avoid having to deal with rate limit headers.
         ModrinthClient {
             client: ApiClientBuilder::new(295, MODRINTH_BASE_URL.to_owned()).build(),
         }
