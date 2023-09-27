fdbe9ea47e8d0c672061507dadcc9030f9915857
Maxwell L-T <maxwell.lt@live.com>
2023-09-26 20:30:31 -0400

    Add basic Modrinth API call

--- crates/api_client/src/modrinth.rs
+++ crates/api_client/src/modrinth.rs
@@ -1,3 +1,5 @@
+use self::model::Project;
+
 use super::common::{ApiClient, ApiClientBuilder};
 
 static MODRINTH_BASE_URL: &str = "https://api.modrinth.com/v2";
@@ -15,5 +17,47 @@ impl ModrinthClient {
         ModrinthClient {
             client: ApiClientBuilder::new(295, MODRINTH_BASE_URL.to_owned()).build(),
         }
+    }
+
+    pub fn get_mod_info(&self, id_or_slug: &str) -> Option<Project> {
+        self.client.get(&format!("/project/{id_or_slug}"), vec![]).ok().and_then(|r| r.into_json().ok())
+    }
+}
+
+mod model {
+use serde::{Serialize, Deserialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Project {
+        pub slug: String,
+        pub title: String,
+        pub client_side: Sided,
+        pub server_side: Sided,
+    }
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(rename_all = "lowercase")]
+    pub enum Sided {
+        Required,
+        Optional,
+        Unsupported,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    static APPLESKIN_ID: &str = "EsAfCjCV";
+    use crate::modrinth::model::Sided;
+
+    use super::*;
+
+    #[test]
+    fn get_mod_info() {
+        let client = ModrinthClient::new();
+        let project = client.get_mod_info(APPLESKIN_ID).unwrap();
+        assert_eq!(project.slug, "appleskin");
+        assert_eq!(project.title, "AppleSkin");
+        assert_eq!(project.client_side, Sided::Required);
+        assert_eq!(project.server_side, Sided::Required);
     }
 }
