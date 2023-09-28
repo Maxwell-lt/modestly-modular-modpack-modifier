8e4c93ef8fa2f41c8639d8971f67acaf1b4ece33
Maxwell L-T <maxwell.lt@live.com>
2023-09-28 17:53:16 -0400

    Implement basic API operations for Modrinth

--- crates/api_client/src/modrinth.rs
+++ crates/api_client/src/modrinth.rs
@@ -1,4 +1,4 @@
-use self::model::Project;
+use self::model::{Project, Version};
 
 use super::common::{ApiClient, ApiClientBuilder};
 
@@ -9,6 +9,28 @@ pub struct ModrinthClient {
     client: ApiClient,
 }
 
+/// API client for Modrinth.
+///
+/// This client applies a rate limit to requests, so applications should instantiate a single
+/// [`ModrinthClient`] and clone copies as needed, to ensure the rate limits are not exceeded.
+/// Internal fields are contained within a [`std::sync::Arc`].
+///
+/// # Examples
+///
+/// ```
+/// use api_client::modrinth::*;
+/// use api_client::modrinth::model::*;
+/// // Basic usage
+/// let client = ModrinthClient::new();
+/// let mod_info: Project = client.get_mod_info("EsAfCjCV").unwrap();
+/// assert_eq!(mod_info.title, "AppleSkin");
+///
+/// // Clone a single client to ensure rate limits are maintained
+/// let client2 = client.clone();
+/// std::thread::spawn(move || {
+///     client2.get_version("Tsz4BT2X").unwrap();
+/// });
+/// ```
 impl ModrinthClient {
     /// Get a [`ModrinthClient`] that uses the official Modrinth API.
     pub fn new() -> Self {
@@ -19,12 +41,69 @@ impl ModrinthClient {
         }
     }
 
+    /// Get mod info from Modrinth, given either a project slug or base-62 numeric ID.
+    ///
+    /// Endpoint: /project/{id|slug}
     pub fn get_mod_info(&self, id_or_slug: &str) -> Option<Project> {
         self.client
             .get(&format!("/project/{id_or_slug}"), vec![])
             .ok()
             .and_then(|r| r.into_json().ok())
     }
+
+    /// Get version list of a mod from Modrinth, given either a project slug or base-62 numeric ID.
+    /// Can optionally filter by mod loader and game version.
+    ///
+    /// Endpoint: /project/{id|slug}/versions
+    pub fn get_mod_versions(&self, id_or_slug: &str, loaders: Option<&[&str]>, game_versions: Option<&[&str]>) -> Option<Vec<Version>> {
+        let mut params = vec![];
+        if let Some(l) = format_params(loaders) {
+            println!("Adding loaders");
+            params.push(("loaders", l));
+        }
+        if let Some(g) = format_params(game_versions) {
+            println!("Adding game_version");
+            params.push(("game_versions", g));
+        }
+        self.client
+            .get(
+                &format!("/project/{id_or_slug}/version"),
+                params
+                    .iter()
+                    // Convert a Vec<(&str, String)> to a Vec<(&str, &str)> to match ureq's API
+                    .map(|&(x, ref y)| (x, &y[..]))
+                    .collect::<Vec<_>>(),
+            )
+            .map_err(|e| {
+                println!("{e:?}");
+                e
+            })
+            .ok()
+            .and_then(|r| {
+                r.into_json()
+                    .map_err(|e| {
+                        println!("{e:?}");
+                        e
+                    })
+                    .ok()
+            })
+    }
+
+    /// Get single version from Modrinth, given its base-62 numeric ID.
+    ///
+    /// Endpoint: /version/{id}
+    pub fn get_version(&self, id: &str) -> Option<Version> {
+        self.client.get(&format!("/version/{id}"), vec![]).ok().and_then(|r| r.into_json().ok())
+    }
+}
+
+/// Format list of items for use as an array query parameter.
+/// For query parameters that are accepted as an array,
+/// Modrinth requires this formatting: `["forge","fabric","quilt"].`
+fn format_params(params: Option<&[&str]>) -> Option<String> {
+    params
+        .and_then(|a| a.iter().map(|l| format!("\"{l}\"")).reduce(|acc, loader| format!("{acc},{loader}")))
+        .map(|a| format!("[{a}]"))
 }
 
 impl Default for ModrinthClient {
@@ -33,7 +112,7 @@ impl Default for ModrinthClient {
     }
 }
 
-mod model {
+pub mod model {
     use serde::{Deserialize, Serialize};
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -42,6 +121,7 @@ mod model {
         pub title: String,
         pub client_side: Sided,
         pub server_side: Sided,
+        pub id: String,
     }
 
     #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -51,11 +131,47 @@ mod model {
         Optional,
         Unsupported,
     }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Version {
+        pub name: String,
+        pub version_number: String,
+        pub game_versions: Vec<String>,
+        pub version_type: VersionType,
+        pub loaders: Vec<String>,
+        pub id: String,
+        pub project_id: String,
+        pub files: Vec<VersionFile>,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct VersionFile {
+        pub hashes: VersionFileHashes,
+        pub url: String,
+        pub filename: String,
+        pub primary: bool,
+        pub size: u64,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct VersionFileHashes {
+        pub sha512: String,
+        pub sha1: String,
+    }
+
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+    #[serde(rename_all = "lowercase")]
+    pub enum VersionType {
+        Release,
+        Beta,
+        Alpha,
+    }
 }
 
 #[cfg(test)]
 mod tests {
     static APPLESKIN_ID: &str = "EsAfCjCV";
+    static APPLESKIN_1_12_VERSION_ID: &str = "Tsz4BT2X";
     use crate::modrinth::model::Sided;
 
     use super::*;
@@ -68,5 +184,47 @@ mod tests {
         assert_eq!(project.title, "AppleSkin");
         assert_eq!(project.client_side, Sided::Required);
         assert_eq!(project.server_side, Sided::Required);
+    }
+
+    #[test]
+    fn mod_info_404() {
+        let client = ModrinthClient::new();
+        let project = client.get_mod_info("this-mod-does-not-exist-abcdefg");
+        assert!(project.is_none());
+    }
+
+    #[test]
+    fn get_mod_versions() {
+        let client = ModrinthClient::new();
+        let versions = client.get_mod_versions(APPLESKIN_ID, None, None).unwrap();
+        assert_eq!(
+            versions
+                .iter()
+                .filter(|v| v.game_versions.iter().any(|v| v == "1.12.2"))
+                .next()
+                .unwrap()
+                .version_number,
+            "1.0.14+mc1.12"
+        );
+    }
+
+    #[test]
+    fn test_format_params() {
+        assert_eq!(format_params(Some(&["1.12.2"])).unwrap(), "[\"1.12.2\"]");
+        assert_eq!(format_params(Some(&["fabric", "quilt"])).unwrap(), "[\"fabric\",\"quilt\"]");
+    }
+
+    #[test]
+    fn filter_versions() {
+        let client = ModrinthClient::new();
+        let versions = client.get_mod_versions(APPLESKIN_ID, Some(&["forge"]), Some(&["1.12.2"])).unwrap();
+        assert_eq!(versions.len(), 1);
+    }
+
+    #[test]
+    fn get_version() {
+        let client = ModrinthClient::new();
+        let version = client.get_version(APPLESKIN_1_12_VERSION_ID).unwrap();
+        assert_eq!(version.version_number, "1.0.14+mc1.12");
     }
 }
