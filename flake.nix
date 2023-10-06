a377defebac6385740528601dd4b8a65848fff62
Maxwell L-T <maxwell.lt@live.com>
2023-10-06 07:08:33 -0400

    Bump version number

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
 	defaultPackage = rustPlatform.buildRustPackage {
 	  pname = "modestly-modular-modpack-modifier";
-	  version = "0.1.1";
+	  version = "0.2.1";
 
 	  src = builtins.filterSource
 	    (path: type: type != "symlink" && baseNameOf path != "target")
