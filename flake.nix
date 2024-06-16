19723f07d346a52a1768d5511b22fd663035a152
Maxwell L-T <maxwell.lt@live.com>
2024-06-15 23:07:37 -0400

    chore: bump dependencies, update version

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
 	defaultPackage = rustPlatform.buildRustPackage {
 	  pname = "modestly-modular-modpack-modifier";
-	  version = "0.5.2";
+	  version = "0.5.5";
 
 	  src = builtins.filterSource
 	    (path: type: type != "symlink" && baseNameOf path != "target")
